b504a5211fc6fc6dcfa10650169b037b0cde4e89
Amna2011 <amnataha2011@gmail.com>
2024-05-07 11:58:46 -0700

    my commit AT

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,6 @@
 #!/usr/bin/env ruby
-
+argument = ARGV[0]
+sender = argument.match(/\[from:([^\]]+)\]/)&.captures&.first
+receiver = argument.match(/\[to:([^\]]+)\]/)&.captures&.first
+flags = argument.match(/\[flags:([^\]]+)\]/)&.captures&.first
+puts "#{sender},#{receiver},#{flags}"
